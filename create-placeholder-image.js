78b60e2acb88123f126665ee0323837b20cb7ac9
colorlessenergy <brianmunozdev@gmail.com>
2021-01-31 09:50:44 -0800

    add functionality to add color to the canvas

--- create-placeholder-image.js
+++ create-placeholder-image.js
@@ -2,13 +2,14 @@
   const form = document.querySelector('#form-placeholder-dimensions');
   const widthInput = form.width;
   const heightInput = form.height;
+  const colorPickerInput = form['color-picker'];
 
   let canvas = document.querySelector('#canvas');
   let context = canvas.getContext('2d');
   canvas.width = widthInput.value;
   canvas.height = heightInput.value;
+  context.fillStyle = colorPickerInput.value;
   context.fillRect(0, 0, widthInput.value, heightInput.value);
-
 
   widthInput.addEventListener('keyup', (ev) => {
     let widthInputValue = ev.target.value;
@@ -22,9 +23,15 @@
     updateCanvasDimensions(widthInputValue, heightInputValue);
   });
 
+  colorPickerInput.addEventListener('input', (ev) => {
+    context.fillStyle = colorPickerInput.value;
+    context.fillRect(0, 0, widthInput.value, heightInput.value);
+  });
+
   function updateCanvasDimensions (width, height) {    
     canvas.width = width;
     canvas.height = height;
+    context.fillStyle = colorPickerInput.value;
     context.fillRect(0, 0, width, height);
   }
 })();
